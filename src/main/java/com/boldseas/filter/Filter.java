ac3ac88ccebcd342b7de2f04bd0b8ee2e9bf05fe
jerry <jerry.zhang@boldseas.com>
2016-12-17 16:10:01 +0800

    lamda expression

--- src/main/java/com/boldseas/filter/Filter.java
+++ src/main/java/com/boldseas/filter/Filter.java
@@ -1,32 +1,14 @@
 package com.boldseas.filter;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by jerry on 12/17/16.
  */
 public class Filter {
     public List<Product> findBySpec(List<Product> products, ProductSpec spec) {
-        List<Product> results = new ArrayList<>();
-        for (Product p: products) {
-            if (spec.satisfy(p)){
-                results.add(p);
-            }
-
-        }
+        List<Product> results = products.stream().filter(spec::satisfy).collect(Collectors.toList());
         return results;
-    }
-
-    public List<Product> findLtWeight(List<Product> products, int weight) {
-        List<Product> results = new ArrayList<>();
-        for (Product p: products) {
-            if (p.getWeight() <  weight){
-                results.add(p);
-            }
-
-        }
-        return results;
-
     }
 }
--- src/main/java/com/boldseas/filter/ProductSpec.java
+++ src/main/java/com/boldseas/filter/ProductSpec.java
@@ -7,64 +7,38 @@ public interface ProductSpec {
     boolean satisfy(Product p);
 
     static ProductSpec color(Color color){
-        return new ProductSpec() {
-            @Override
-            public boolean satisfy(Product p) {
-                return p.getColor() == color;
-            }
-        };
+        return p -> p.getColor() == color;
     }
     static ProductSpec ltWeight(int weight){
-        return new ProductSpec() {
-            @Override
-            public boolean satisfy(Product p) {
-                return p.getWeight() < weight;
-            }
-        };
+        return p -> p.getWeight() < weight;
     }
     static ProductSpec and(ProductSpec... specs){
-        return new ProductSpec() {
-            @Override
-            public boolean satisfy(Product p) {
-                for (ProductSpec spec: specs) {
-                    if (!spec.satisfy(p)){
-                        return false;
-                    }
+        return p -> {
+            for (ProductSpec spec: specs) {
+                if (!spec.satisfy(p)){
+                    return false;
                 }
-                return true;
             }
+            return true;
         };
     }
     static ProductSpec or(ProductSpec... specs){
-        return new ProductSpec() {
-            @Override
-            public boolean satisfy(Product p) {
-                for (ProductSpec spec: specs) {
-                    if (spec.satisfy(p)){
-                        return true;
-                    }
+        return p -> {
+            for (ProductSpec spec: specs) {
+                if (spec.satisfy(p)){
+                    return true;
                 }
-                return false;
             }
+            return false;
         };
     }
 
     static ProductSpec not(ProductSpec spec){
-        return new ProductSpec() {
-            @Override
-            public boolean satisfy(Product p) {
-                return !spec.satisfy(p);
-            }
-        };
+        return p -> !spec.satisfy(p);
     }
 
     static ProductSpec always(boolean bool){
-        return new ProductSpec() {
-            @Override
-            public boolean satisfy(Product p) {
-                return bool;
-            }
-        };
+        return p -> bool;
     }
 
 }
